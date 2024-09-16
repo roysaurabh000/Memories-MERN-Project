d61597836019cd820bfd881eaadcd080964e0a04
Saurabh Roy <saurabhroy@Saurabhs-MacBook-Air.local>
2024-09-15 23:11:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Memories-MERN-Project
+A Full Stack MERN Application called "Memories" that allows users to post interesting events that happened in their lives. Implemented Using React, Node.js, Express &amp; MongoDB.
+
+
+
+
+- Go to the server folder.
+- Create a ".env" file enter the SECRETS in the format as in the file .env.example. 
+- Open terminal in the cloned repository.
+- Type `cd client`.
+- Type `npm install`.
+- Type `npm start`.
+
+
+- Go to the server folder.
+- Create a ".env" file enter the SECRETS in the format as in the file .env.example.  
+- Open terminal in the cloned repository.
+- Type `cd server`.
+- Type `npm install`.
+- Type `npm start`.
+
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { signin, signup } from "../../actions/auth";
+import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+import Input from "./Input";
+import useStyles from "./styles";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const Auth = () => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    if (isSignup) {
+      dispatch(signup(formData, history));
+    } else {
+      dispatch(signin(formData, history));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  };
+
+  const googleSuccess = async (res) => {
+    const token = res?.credential;
+    const decoded = jwt_decode(res?.credential);
+    // console.log(result); // ? -> helps in not giving error if the obj does not exist
+    const { email, family_name, given_name, name, sub: googleId, picture } = decoded;
+    const result = { email, family_name, given_name, name, googleId, picture };
+
+    try {
+      dispatch({ type: "AUTH", data: { result, token } });
+      history.push("/posts");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // global google
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: googleSuccess,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("googleSignIn"), {
+      theme: "",
+      size: "large",
+    });
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+              </>
+            )}
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            {isSignup ? "Sign Up" : "Sign In"}
+          </Button>
+          <Button className={classes.googleButton} id="googleSignIn" fullWidth variant="text">
+            Google Sign In
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>{isSignup ? "Already have an account? Sign in" : "Don't have an account? Sign Up"}</Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
